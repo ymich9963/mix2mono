cfb4c6916316a1d50595195da29501d789c816f2
ymich9963 <gmichael834@gmail.com>
2025-09-01 17:24:00 +0100

    Removed comment.

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "mix2mono.h"
 
 //TODO: Test different formats and encodings. Works with WAVE.
-//FIX: Does not like paths as file inputs
 
 int main (int argc, char** argv) {
     SF_INFO sf_info;        // File info
